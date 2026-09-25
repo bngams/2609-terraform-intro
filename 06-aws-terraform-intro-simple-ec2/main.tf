4abd733d76f42a3c2f959459e3b549c2b08c4c09
Boris Ngam <boris@deltavia.com>
2026-09-25 13:48:10 +0200

    gitlab pipeline scenario

--- 06-aws-terraform-intro-simple-ec2/main.tf
+++ 06-aws-terraform-intro-simple-ec2/main.tf
@@ -3,6 +3,16 @@ data "aws_vpc" "default" {
   default = true
 }
 
+# Data source for AMI (Amazon Machine Image)
+data "aws_ami" "ami" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 # SECURITY GROUP: allow inbound SSH (22) and HTTP (80), all outbound
 resource "aws_security_group" "ec2_sg" {
   name        = "${var.instance_name}-sg"
@@ -40,12 +50,12 @@ resource "aws_security_group" "ec2_sg" {
 
 # EC2 INSTANCE - T3 Micro
 resource "aws_instance" "simple_ec2" {
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
   # name of an existing EC2 key pair in AWS
-  key_name               = var.key_name
+  # key_name               = var.key_name
   # attach the security group to the instance
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  # vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
     Name = var.instance_name
